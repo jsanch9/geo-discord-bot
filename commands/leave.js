d5f28feb38be16dbc1cb1a7c7f5da41e8e73e3ae
Javier Sanchez <jsanchez@goguardian.com>
2021-07-10 23:18:06 -0700

    leave messages

--- commands/leave.js
+++ commands/leave.js
@@ -7,9 +7,13 @@ module.exports ={
     name: "leave",
     description: "the bot leaves the voice channel",
     execute(message,stay,VC){
-        if (!message.guild.voice.channel) return;
+        if (!message.guild.voice.channel){
+            message.channel.send("Leave from where fucking stupid stupid.")    
+            return;
+        } 
 
         global.stay = false;
         VC.leave();
+        message.channel.send("Ok then bye, lick my ass.")
     }
 }
